e7c8fc2a6d419def1bbe437f2eb9530e0c4462d9
Vinny Martinez <vinny@vinnymartinez.com>
2017-01-25 08:42:21 -0500

    dunzo

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true
+}
+
+function sayHey() {
+  return 'Hey!'
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(icecream) {
+  return `I love ${icecream}`
+}
+
+function shouting(strang) {
+  return strang.toUpperCase()
+}
+
+function roundDown(nummy) {
+  return Math.floor(nummy)
+}
